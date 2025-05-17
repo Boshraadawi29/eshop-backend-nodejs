e42b26ba654ad605e2106945e4725ac6bb1c003c
boshraadawi29 <boshraadawi4@gmail.com>
2025-05-17 20:26:33 +0300

    Formating stuff

--- models/product.js
+++ models/product.js
@@ -1,61 +1,62 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
   name: {
-    type: String, 
-    required: true
-  }, 
-  description:{
     type: String,
-    required: true
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
   },
   richDescription: {
-    type: String, 
-    default: ''
-  }, 
-  image: {
-    type: String, 
-    default: ''
+    type: String,
+    default: "",
   },
-  images: [{
-    type: String
-  }],
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
   brand: {
-    type: String, 
-    default: ''
-  }, 
+    type: String,
+    default: "",
+  },
   price: {
-    type: Number, 
-    default: 0
-  }, 
-  category:{
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Category',
-    required: true
-  }, 
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
   countInStock: {
     type: Number,
     required: true,
-    min: 0, 
-    max: 200
-  }, 
+    min: 0,
+    max: 200,
+  },
   rating: {
-    type: Number, 
-    default: 0
-  }, 
-  numReveiws: {
-    type: Number, 
-    default: 0
-  }, 
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
   isFeatured: {
-    type: Boolean, 
-    default: false
-  }, 
+    type: Boolean,
+    default: false,
+  },
   dateCreated: {
-    type: Date, 
-    default: Date.now
-  }
+    type: Date,
+    default: Date.now,
+  },
 });
 
-exports.Product = mongoose.model('Product', productSchema);
+exports.Product = mongoose.model("Product", productSchema);
