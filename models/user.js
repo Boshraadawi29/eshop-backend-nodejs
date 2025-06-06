57a98e9bfdd939f445e8b3d82df90c4dce0e5b4d
boshraadawi29 <boshraadawi4@gmail.com>
2025-06-06 22:30:16 +0300

    feat: add user schema

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter a full name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'please enter your email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please enter your phone number'],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  street: {
+    type: String,
+    default: '',
+  },
+  apartment: {
+    type: String,
+    default: '',
+  },
+  zip: {
+    type: String,
+    default: '',
+  },
+  city: {
+    type: String,
+    default: '',
+  },
+  country: {
+    type: String,
+    default: '',
+  },
+});
+
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set('toString', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+    //   delete ret.password;
+  },
+});
+
+exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
