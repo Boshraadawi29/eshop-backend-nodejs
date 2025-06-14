7670bd5816e56002a2be8aa13ecb7145c8cdad7b
boshraadawi29 <boshraadawi4@gmail.com>
2025-06-14 19:16:53 +0300

    Implement bcrypt password hashing

--- routers/users.js
+++ routers/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { User } = require('../models/user');
 
@@ -23,32 +24,42 @@ router.post(`/`, async (req, res) => {
     });
   }
 
-  const user = new User({
-    name,
-    email,
-    password,
-    phone,
-    isAdmin,
-    street,
-    apartment,
-    zip,
-    city,
-    country,
-  });
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      isAdmin,
+      street,
+      apartment,
+      zip,
+      city,
+      country,
+    });
 
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
-  if (!savedUser) {
-    return res.status(500).json({
+    if (!savedUser) {
+      return res.status(500).json({
+        success: false,
+        message: 'Can not create user',
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      user: savedUser,
+    });
+  } catch (errot) {
+    res.status(500).json({
       success: false,
-      message: 'Can not create user',
+      message: 'Server error during user creation',
+      error,
     });
   }
-
-  res.status(201).json({
-    success: true,
-    user: savedUser,
-  });
 });
 
 module.exports = router;
