532756d2d7679885940891487790b838560aa3ee
boshraadawi29 <boshraadawi4@gmail.com>
2025-06-28 19:47:05 +0300

    catch the validation errors

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -2,8 +2,13 @@ function errorHandler(err, req, res, next) {
   console.error('🔥 Unexpected Error:', err.stack);
   if(err.name === 'UnauthorizedError'){
 
-    return res.status(500).json({success: false, message: 'User is not autherized'})
+    return res.status(401).json({success: false, message: 'User is not autherized'})
   }
+
+  if(err.name === 'validationError'){
+    return res.status(401).json({success: false, message: err })
+  }
+  
   res.status(500).json({ success: false, message: 'Something went wrong!' });
 }
 
