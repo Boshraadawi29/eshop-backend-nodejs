0d4e73d161b93b820b49af275721526606dd5f23
boshraadawi29 <boshraadawi4@gmail.com>
2025-07-13 21:37:07 +0300

    feat: add checkAdmin middleware to restrict access

--- helpers/checkAdmin.js
+++ helpers/checkAdmin.js
@@ -1,7 +1,7 @@
 // middlewares/checkAdmin.js
 module.exports = function checkAdmin(req, res, next) {
-  if (req.user && req.user.isAdmin) {
-    next(); // allowed
+  if (req.user && req.user.isAdmin === true) {
+    next(); // continue to route handler
   } else {
     res.status(403).json({ message: 'Access denied. Admins only.' });
   }
