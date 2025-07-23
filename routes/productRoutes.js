ad6a67e6bc1d2011f010baaabf1b2112e672ec56
boshraadawi29 <boshraadawi4@gmail.com>
2025-07-23 18:53:57 +0300

    Apply checkAdmin validation to /newproduct, PUT /, and DELETE / routes to restrict access to admin users only

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const checkAdmin = require('../helpers/checkAdmin')
 
 // Routes mapping to controller methods
-router.post('/newproduct', productController.createProduct);
+router.post('/newproduct', checkAdmin, productController.createProduct);
 router.get('/productlist', productController.getAllProducts);
 router.get('/productnames', productController.getProductNames);
-router.put('/', productController.updateProduct);
-router.delete('/', productController.deleteProduct);
+router.put('/', checkAdmin, productController.updateProduct);
+router.delete('/',checkAdmin, productController.deleteProduct);
 router.get('/count', productController.getProductCount);
 router.get('/featured', productController.getFeaturedProducts);
 router.get('/filtered', productController.getFilteredProducts);
